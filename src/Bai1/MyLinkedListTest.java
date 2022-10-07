74f5f26a6181fb33d110fb2cdae0d6471bfd2f4e
khanhdzx <khanhvannguyen2303@gmail.com>
2022-10-07 09:55:05 +0700

    ja

---
+++ src/Bai1/MyLinkedListTest.java
@@ -0,0 +1,20 @@
+package Bai1;
+
+class MyLinkedListTest {
+    public static void main(String[] args) {
+
+        MyLinkedList test = new MyLinkedList(10);
+        test.add(1, 2);
+        test.add(1, 2);
+        test.add(1, 2);
+        test.add(1, 2);
+
+        test.addFirst(3);
+        test.addFirst(5);
+        test.addFirst(11);
+
+
+        test.printList();
+
+    }
+}
